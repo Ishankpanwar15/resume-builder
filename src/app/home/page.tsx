234008bd99e72c4aff8ae1592a2b453bb7f3ab74
om-khorwal <amankhorwal250@gmail.com>
2025-05-27 14:46:03 +0530

    all work

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,21 +2,33 @@ import React from "react";
 
 const home = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold mb-4">Welcome to the Home Page</h1>
-      <p className="text-lg">This is a simple home page built with React and Tailwind CSS.</p>
-      <div className="mt-6 flex space-x-4">
-        <button>
-          <a href="/create-resume" className="mt-4 px-6 py-2 bg-violet-500 text-white rounded hover:bg-violet-600">
-          Create Resume
-          </a>
-        </button>
-         <button>
-          <a href="/login" className="mt-4 px-6 py-2 bg-violet-500 text-white rounded hover:bg-violet-600">
-          Login
-          </a>
-        </button>
-      </div>
+    <div className="flex bg-white">
+        <div className="flex flex-col w-1/3 justify-center h-screen">
+          <img src="https://i.postimg.cc/PfSjTH6G/Group-2.png" alt="Decorative"
+               className="w-full max-h-full ibject-contain p-4"
+          />
+        </div>
+        <div className="flex flex-col items-center justify-center h-screen w-2/3 bg-[#f3ecf3] rounded-tl-[8rem]
+">
+        <div className="p-4 ">
+          <h1 className="text-black text-[7rem] font-black leading-none tracking-tight">
+            Welcome to <br /><span className="text-violet-600">Resume Builder</span>
+          </h1>
+        </div>
+
+
+          <p className="text-gray-600 text-3xl">Where Modern Resume Meet <span className="text-violet-600">Career Wins</span></p>
+
+          <div className="flex justify-start space-x-4 mt-6">
+            <button className="bg-violet-500 text-white px-6 py-2 rounded hover:bg-violet-600">
+              Create Resume
+            </button>
+            <button className="bg-violet-500 text-white px-6 py-2 rounded hover:bg-violet-600">
+              Login
+            </button>
+          </div>
+
+        </div>
     </div>
   );
 }
