f7bf13c7b19e11ea630cd61869ef71131037a99e
Ishankpanwar15 <15ishank02@gmail.com>
2025-05-27 11:02:14 +0530

    Refactor auth : replace login/signup APIs with NextAuth

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -20,7 +20,7 @@ export default function LoginPage() {
         if (res.ok) {
             router.push('/dashboard')
       } else {
-        setError(data.message)add 
+        setError(data.message) 
       }
    }
 
