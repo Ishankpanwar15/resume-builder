d4b7ce3793ff226e6d9a177fbe1f1a230eff1994
Ishankpanwar15 <15ishank02@gmail.com>
2025-05-27 10:41:40 +0530

    uploading Next file

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -25,25 +25,46 @@ export default function LoginPage() {
    }
 
    return(
-    <form onSubmit={handleLogin}>
-        <h2>Login</h2>
-        {error && <p style={{color: 'red'}}>{error}</p>}
-        <input 
-            type="email"
-            placeholder='Email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-        />
-        <input 
-            type="password"
-            placeholder='Password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-        />
-        <button type='submit'>Login</button>
-        <p>Don't have an account? <a href="/signup">Sign up</a></p>
-    </form>
+    <div
+        style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            backgroundColor: '#f5f5f5'
+        }}>
+        <form 
+            onSubmit={handleLogin}
+            style={{
+                backgroundColor: 'white',
+                padding: '2rem',
+                borderRadius: '8px',
+                width: '100%',
+                maxWidth: '400px',
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '1rem',
+            }}
+        >
+            <h2 style={{ textAlign:'center'}}>Login</h2>
+            {error && <p style={{color: 'red'}}>{error}</p>}
+            <input 
+                type="email"
+                placeholder='Email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                />
+            <input 
+                type="password"
+                placeholder='Password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                />
+            <button type='submit'>Login</button>
+            <p>Don't have an account? <a href="/signup">Sign up</a></p>
+        </form>
+    </div>
    )
 }
